f75fe86419bf22c979b1db9f749dc2a9d9b0b280
NachoLR <ignaciolopezrivas@gmail.com>
2021-04-25 20:41:43 +0200

    Notification system HECHO, falta que las notificaciones popUp funcionen bien pero mejor meterles mano despues de que el arte esté puesto.

--- Assets/Scripts/Notificaciones/Notification_SO.cs
+++ Assets/Scripts/Notificaciones/Notification_SO.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[CreateAssetMenu(fileName = "Notification", menuName = "SystemNotification/Notification", order = 1)]
 public class Notification_SO : ScriptableObject
 {
-    Text notificationTitle;
+    [SerializeField] string title;
+    [SerializeField] string body;
+
+    public string getTitle() { return title; }
+    public string getBody() { return body; }
 
 }
